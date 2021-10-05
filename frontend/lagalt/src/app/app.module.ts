432dbad5c42c3eee8ae2c62d039a217f74d482a0
emilons <emilons@hotmail.com>
2021-10-05 10:32:01 +0200

    #3 made main page

--- frontend/lagalt/src/app/app.module.ts
+++ frontend/lagalt/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPage } from './pages/main/main.page';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPage
   ],
   imports: [
     BrowserModule,
---
+++ frontend/lagalt/src/app/pages/main/main.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.page.html',
+  styleUrls: ['./main.page.css']
+})
+export class MainPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
