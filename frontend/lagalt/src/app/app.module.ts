cfa3d8fcf5cda1450bc155d4902b0fcededf80d2
emilons <emilons@hotmail.com>
2021-10-05 10:33:32 +0200

    3 made main search component

--- frontend/lagalt/src/app/app.module.ts
+++ frontend/lagalt/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPage } from './pages/main/main.page';
+import { MainSearchComponent } from './components/main-search/main-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPage
+    MainPage,
+    MainSearchComponent
   ],
   imports: [
     BrowserModule,
