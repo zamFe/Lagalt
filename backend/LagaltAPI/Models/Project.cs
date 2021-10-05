0d0431083c0e4c5a097836da1754f23830c84522
LasseSormo <lasse.soermo@mailbox.org>
2021-10-05 15:27:56 +0200

    Added basic data models for code-first SQL building

---
+++ backend/LagaltAPI/Models/Project.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace LagaltAPI.Models
+{
+    /// <summary>Simple data model representing a project at Lagalt</summary>
+    public class Project
+    {
+        public int Id { get; set; }
+
+        public Profession Profession { get; set; }
+
+        public ICollection<Message> Messages { get; set; }
+
+        public ICollection<UserProject> UserProjects { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+
+        public string Source { get; set; }
+
+        public string Progress { get; set; }
+    }
+}
