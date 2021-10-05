47e85a3e87973f41b1033eabdba4c312e1b7dc10
emilons <emilons@hotmail.com>
2021-10-05 10:37:42 +0200

    #3 made main filter

--- frontend/lagalt/src/app/app.module.ts
+++ frontend/lagalt/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPage } from './pages/main/main.page';
 import { MainSearchComponent } from './components/main-search/main-search.component';
+import { MainFilterComponent } from './components/main-filter/main-filter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPage,
-    MainSearchComponent
+    MainSearchComponent,
+    MainFilterComponent
   ],
   imports: [
     BrowserModule,
