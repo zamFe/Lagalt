0e75c7330a3ff5272244b92325667e6206cfd532
emilons <emilons@hotmail.com>
2021-10-05 11:29:42 +0200

    #3 added main page to router

--- frontend/lagalt/src/app/app-routing.module.ts
+++ frontend/lagalt/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPage } from "./pages/main/main.page";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+      path: '',
+      pathMatch: 'full',
+      redirectTo: 'main'
+  },
+  {
+      path: 'main',
+      component: MainPage
+  }
+
+
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
