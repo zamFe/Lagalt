22f35a5471c288b5f6f639c13d7472352b5d134c
Edlix <56119942+Edlix@users.noreply.github.com>
2021-10-06 15:12:57 +0200

    Fixed app.module.ts

--- frontend/lagalt/src/app/app.module.ts
+++ frontend/lagalt/src/app/app.module.ts
@@ -12,7 +12,6 @@ import { MainListOfProjectsComponent } from './components/main-list-of-projects/
 import { MainFilterOptionComponent } from './components/main-filter-option/main-filter-option.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProfilePage } from './pages/profile/profile.page';
-import { ProfileImageComponent } from './components/profile-image/profile-image.component';
 import { ProfileUserInfoComponent } from './components/profile-user-info/profile-user-info.component';
 import { ProfileSettingsComponent } from './components/profile-settings/profile-settings.component';
 import {ProjectPage} from "./pages/project/project.page";
@@ -35,7 +34,6 @@ import { ProjectStatusComponent } from './components/project-status/project-stat
     MainFilterOptionComponent,
     NavbarComponent,
     ProfilePage,
-    ProfileImageComponent,
     ProfileUserInfoComponent,
     ProfileSettingsComponent,
     ProjectPage,
