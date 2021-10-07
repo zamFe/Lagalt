6ff8cda0f452185997cc5e64c55869826bbfee96
Edlix <56119942+Edlix@users.noreply.github.com>
2021-10-07 14:26:59 +0200

    #17 feat profile glance skeleton

--- frontend/lagalt/src/app/components/main-list-of-projects-item/main-list-of-projects-item.component.ts
+++ frontend/lagalt/src/app/components/main-list-of-projects-item/main-list-of-projects-item.component.ts
@@ -1,6 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Project } from '../main-list-of-projects/main-list-of-projects.component';
 
+let skillsList = [
+  "python",
+  "drummer",
+  "superhero",
+]
+
 @Component({
   selector: 'app-main-list-of-projects-item',
   templateUrl: './main-list-of-projects-item.component.html',
@@ -13,9 +19,12 @@ export class MainListOfProjectsItemComponent implements OnInit {
     type: '',
     thumbnail: 'white'
   };
+  public skills: string[] = []
   constructor() { }
 
   ngOnInit(): void {
+    this.skills = skillsList
   }
+
 
 }
