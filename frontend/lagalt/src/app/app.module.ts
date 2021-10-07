1beb92cbe59d7c5e1fafb7dfc8489af566f91200
emilons <emilons@hotmail.com>
2021-10-07 09:50:16 +0200

    fixed merge errors

--- frontend/lagalt/src/app/app.module.ts
+++ frontend/lagalt/src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule} from "@angular/forms";
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPage } from './pages/main/main.page';
@@ -37,7 +35,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MainListOfProjectsComponent,
     MainFilterOptionComponent,
     NavbarComponent,
-    MainListOfProjectsItemComponent
+    MainListOfProjectsItemComponent,
     ProfilePage,
     ProfileUserInfoComponent,
     ProfileSettingsComponent,
