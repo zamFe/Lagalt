f4f548cfb90407e4eb0abca2735b626ea0e6a60a
zamFe <zamfel18@student.kristiania.no>
2021-10-07 14:35:37 +0200

    #23 FEAT: Implemented ProjectService

---
+++ backend/LagaltAPI/Repositories/ProjectService.cs
@@ -0,0 +1,63 @@
+﻿using LagaltAPI.Context;
+using LagaltAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LagaltAPI.Repositories
+{
+    public class ProjectService : IService<Project>
+    {
+
+        private readonly LagaltContext _context;
+
+        public ProjectService(LagaltContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Project>> GetAllAsync()
+        {
+            return await _context.Projects
+                .Include(p => p.Messages)
+                .Include(p => p.UserProjects)
+                .ToListAsync();
+        }
+
+        public async Task<Project> GetByIdAsync(int Id)
+        {
+            return await _context.Projects
+                .Include(p => p.Messages)
+                .Include(p => p.UserProjects)
+                .Where(p => p.Id == Id)
+                .FirstAsync();
+        }
+
+        public async Task<Project> AddAsync(Project entity)
+        {
+            _context.Projects.Add(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var user = await _context.Projects.FindAsync(id);
+            _context.Projects.Remove(user);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Project entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public bool EntityExists(int id)
+        {
+            return _context.Projects.Any(p => p.Id == id);
+        }
+    }
+}
