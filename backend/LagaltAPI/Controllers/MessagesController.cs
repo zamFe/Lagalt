276d5cf669922617a2213f5467027384da1f5975
LasseSormo <lasse.soermo@mailbox.org>
2021-10-08 10:28:32 +0200

    #29 Improve database documentation

--- backend/LagaltAPI/Controllers/MessagesController.cs
+++ backend/LagaltAPI/Controllers/MessagesController.cs
@@ -1,30 +1,31 @@
 ﻿using AutoMapper;
-using LagaltAPI.Context;
 using LagaltAPI.Models;
 using LagaltAPI.Models.DTOs.Message;
 using LagaltAPI.Repositories;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace LagaltAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [ApiConventionType(typeof(DefaultApiConventions))]
     public class MessagesController : ControllerBase
     {
-        private readonly MessageService _service;
         private readonly IMapper _mapper;
+        private readonly MessageService _service;
 
-        public MessagesController(MessageService service, IMapper mapper)
+        // Constructor.
+        public MessagesController(IMapper mapper, MessageService service)
         {
-            _service = service;
             _mapper = mapper;
+            _service = service;
         }
 
+        /// <summary> Fetches all available messages from the database. </summary>
+        /// <returns> An enumerable containing read-specific DTOs of the messages. </returns>
         // GET: api/Messages
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MessageReadDTO>>> GetMessages()
@@ -32,57 +33,68 @@ namespace LagaltAPI.Controllers
             return _mapper.Map<List<MessageReadDTO>>(await _service.GetAllAsync());
         }
 
+        /// <summary> Fetches a message from the database based on id. </summary>
+        /// <param name="id"> The id of the message to retrieve. </param>
+        /// <returns>
+        ///     A read-specific DTO of the message if it is found in the database.
+        ///     If it is not, then NotFound is returned instead.
+        /// </returns>
         // GET: api/Messages/5
         [HttpGet("{id}")]
         public async Task<ActionResult<MessageReadDTO>> GetMessage(int id)
         {
             try
             {
-                var message = await _service.GetByIdAsync(id);
+                var domainMessage = await _service.GetByIdAsync(id);
 
-                if (message != null)
-                    return _mapper.Map<MessageReadDTO>(message);
+                if (domainMessage != null)
+                    return _mapper.Map<MessageReadDTO>(domainMessage);
                 else
                     return NotFound();
             }
-            catch (ArgumentNullException) { return BadRequest(); }
-            catch (InvalidOperationException) { return NotFound(); }
+            catch (InvalidOperationException) {
+                return NotFound();
+            }
         }
 
-        /* TODO - decide whether to support editing messages
+        /* TODO - Decide whether or not editing messages will be supported.
          * 
-        // PUT: api/Messages/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutMessage(int id, Message message)
-        {
-            if (id != message.Id)
-                return BadRequest();
+         * // PUT: api/Messages/5
+         * [HttpPut("{id}")]
+         * public async Task<IActionResult> PutMessage(int id, Message message)
+         * {
+         *    if (id != message.Id)
+         *        return BadRequest();
+         * 
+         *    _context.Entry(message).State = EntityState.Modified;
+         * 
+         *    try
+         *    {
+         *        await _context.SaveChangesAsync();
+         *    }
+         *    catch (DbUpdateConcurrencyException)
+         *    {
+         *        if (!MessageExists(id))
+         *            return NotFound();
+         *        else
+         *            throw;
+         *    }
+         * 
+         *    return NoContent();
+         * }
+         */
 
-            _context.Entry(message).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!MessageExists(id))
-                    return NotFound();
-                else
-                    throw;
-            }
-
-            return NoContent();
-        }
-        */
-
+        /// <summary> Adds a new message entry to the database. </summary>
+        /// <param name="dtoMessage"> A create-specific DTO representing the new message. </param>
+        /// <returns>
+        ///     A read-specific DTO of the message just added to the database on success,
+        ///     or BadRequest on failure.
+        /// </returns>
         // POST: api/Messages
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<Message>> PostMessage(Message dtoMessage)
+        public async Task<ActionResult<MessageReadDTO>> PostMessage(MessageCreateDTO dtoMessage)
         {
-            Message domainMessage = _mapper.Map<Message>(dtoMessage);
+            var domainMessage = _mapper.Map<Message>(dtoMessage);
             await _service.AddAsync(domainMessage);
 
             return CreatedAtAction("GetMessage", 
@@ -90,28 +102,28 @@ namespace LagaltAPI.Controllers
                 _mapper.Map<MessageReadDTO>(domainMessage));
         }
 
-        /* NOT CURRENTLY SUPPORTED */
-        /* 
-        // DELETE: api/Messages/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteMessage(int id)
-        {
-            var message = await _context.Messages.FindAsync(id);
-            if (message == null)
-            {
-                return NotFound();
-            }
-
-            _context.Messages.Remove(message);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool MessageExists(int id)
-        {
-            return _context.Messages.Any(e => e.Id == id);
-        }
-        */
+        /* TODO - Decide whether or not deleting messages will be supported.
+         * 
+         * // DELETE: api/Messages/5
+         * [HttpDelete("{id}")]
+         * public async Task<IActionResult> DeleteMessage(int id)
+         * {
+         *    var message = await _context.Messages.FindAsync(id);
+         *    if (message == null)
+         *    {
+         *        return NotFound();
+         *    }
+         * 
+         *    _context.Messages.Remove(message);
+         *    await _context.SaveChangesAsync();
+         * 
+         *    return NoContent();
+         * }
+         * 
+         * private bool MessageExists(int id)
+         * {
+         *    return _context.Messages.Any(e => e.Id == id);
+         * }
+         */
     }
 }
--- backend/LagaltAPI/Controllers/ProfessionsController.cs
+++ backend/LagaltAPI/Controllers/ProfessionsController.cs
@@ -1,20 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using LagaltAPI.Context;
-using LagaltAPI.Models;
-using AutoMapper;
+﻿using AutoMapper;
 using LagaltAPI.Models.DTOs.Profession;
 using LagaltAPI.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace LagaltAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [ApiConventionType(typeof(DefaultApiConventions))]
     public class ProfessionsController : ControllerBase
     {
         private readonly IMapper _mapper;
@@ -26,6 +22,8 @@ namespace LagaltAPI.Controllers
             _service = service;
         }
 
+        /// <summary> Fetches all available professions from the database. </summary>
+        /// <returns> An enumerable containing read-specific DTOs of the professions. </returns>
         // GET: api/Professions
         [HttpGet]
         public async Task<ActionResult<IEnumerable<ProfessionReadDTO>>> GetProfessions()
@@ -33,22 +31,24 @@ namespace LagaltAPI.Controllers
             return _mapper.Map<List<ProfessionReadDTO>>(await _service.GetAllAsync());
         }
 
+        /// <summary> Fetches a profession from the database based on id. </summary>
+        /// <param name="id"> The id of the profession to retrieve. </param>
+        /// <returns>
+        ///     A read-specific DTO of the profession if it is found in the database.
+        ///     If it is not, then NotFound is returned instead.
+        /// </returns>
         // GET: api/Professions/5
         [HttpGet("{id}")]
         public async Task<ActionResult<ProfessionReadDTO>> GetProfession(int id)
         {
             try
             {
-                var professionDomain = await _service.GetByIdAsync(id);
+                var domainProfession = await _service.GetByIdAsync(id);
 
-                if (professionDomain != null)
-                    return _mapper.Map<ProfessionReadDTO>(professionDomain);
+                if (domainProfession != null)
+                    return _mapper.Map<ProfessionReadDTO>(domainProfession);
                 else
                     return NotFound();
-            }
-            catch (ArgumentNullException)
-            {
-                return BadRequest();
             }
             catch (InvalidOperationException)
             {
@@ -56,55 +56,53 @@ namespace LagaltAPI.Controllers
             }
         }
 
-        /* PUT, POST and DELETE are disabled until support for more professions is added
+        /* PUT, POST and DELETE are disabled until support for more professions is added.
          *
-        * // PUT: api/Professions/5
-        * // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        * [HttpPut("{id}")]
-        * public async Task<IActionResult> PutProfession(int id, Profession profession)
-        * {
-        *     if (id != profession.Id)
-        *         return BadRequest();
-        *
-        *     _context.Entry(profession).State = EntityState.Modified;
-        *
-        *     try
-        *         await _context.SaveChangesAsync();
-        *     catch (DbUpdateConcurrencyException)
-        *     {
-        *         if (!ProfessionExists(id))
-        *             return NotFound();
-        *         else
-        *             throw;
-        *     }
-        *
-        *     return NoContent();
-        * }
-        * 
-        * // POST: api/Professions
-        * // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        * [HttpPost]
-        * public async Task<ActionResult<Profession>> PostProfession(Profession profession)
-        * {
-        *     _context.Professions.Add(profession);
-        *     await _context.SaveChangesAsync();
-        *
-        *     return CreatedAtAction("GetProfession", new { id = profession.Id }, profession);
-        * }
-        *
-        * // DELETE: api/Professions/5
-        * [HttpDelete("{id}")]
-        * public async Task<IActionResult> DeleteProfession(int id)
-        * {
-        *     var profession = await _context.Professions.FindAsync(id);
-        *    if (profession == null)
-        *         return NotFound();
-        * 
-        *     _context.Professions.Remove(profession);
-        *    await _context.SaveChangesAsync();
-        * 
-        *     return NoContent();
-        * }
-        */
+         * // PUT: api/Professions/5
+         * [HttpPut("{id}")]
+         * public async Task<IActionResult> PutProfession(int id, Profession profession)
+         * {
+         *     if (id != profession.Id)
+         *         return BadRequest();
+         *
+         *     _context.Entry(profession).State = EntityState.Modified;
+         *
+         *     try
+         *         await _context.SaveChangesAsync();
+         *     catch (DbUpdateConcurrencyException)
+         *     {
+         *         if (!ProfessionExists(id))
+         *             return NotFound();
+         *         else
+         *             throw;
+         *     }
+         *
+         *     return NoContent();
+         * }
+         * 
+         * // POST: api/Professions
+         * [HttpPost]
+         * public async Task<ActionResult<Profession>> PostProfession(Profession profession)
+         * {
+         *     _context.Professions.Add(profession);
+         *     await _context.SaveChangesAsync();
+         *
+         *     return CreatedAtAction("GetProfession", new { id = profession.Id }, profession);
+         * }
+         *
+         * // DELETE: api/Professions/5
+         * [HttpDelete("{id}")]
+         * public async Task<IActionResult> DeleteProfession(int id)
+         * {
+         *     var profession = await _context.Professions.FindAsync(id);
+         *    if (profession == null)
+         *         return NotFound();
+         * 
+         *     _context.Professions.Remove(profession);
+         *    await _context.SaveChangesAsync();
+         * 
+         *     return NoContent();
+         * }
+         */
     }
 }
