86686fc2f90f0a5dac02eb4ea0e607f319e82f12
zamFe <zamfel18@student.kristiania.no>
2021-10-11 14:02:43 +0200

    Merged Heroku-dev into main

--- backend/LagaltAPI/Program.cs
+++ backend/LagaltAPI/Program.cs
@@ -8,23 +8,26 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using LagaltAPI;
 
 namespace LagaltAPI
 {
     public class Program
     {
         public static void Main(string[] args)
-        {   
+        {
             var config =
                 new ConfigurationBuilder()
                 .AddEnvironmentVariables()
                 .Build();
 
-            CreateHostBuilder(args).Build()
-                .MigrateDatabase() // from custom extension in 'Extensions.cs' 
-                .Run();
+            //CreateWebHostBuilder(args).Build().Run();     //For Testing
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseSetting("https_port", "5000")
+                .UseStartup<Startup>();
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
