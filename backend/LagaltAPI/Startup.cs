18ea0a358519e16cc681b671b68449379b648e02
zamFe <zamfel18@student.kristiania.no>
2021-10-11 11:06:26 +0200

    set up for heroku deployment (hopefully)

--- backend/LagaltAPI/Startup.cs
+++ backend/LagaltAPI/Startup.cs
@@ -48,7 +48,7 @@ namespace LagaltAPI
                     var pgHostPortDb = connUrl.Split("@")[1];
                     var pgHostPort = pgHostPortDb.Split("/")[0];
                     var pgDb = pgHostPortDb.Split("/")[1];
-                    var pgUser = pgUserPass.Split(":")[0].Split("//")[1];
+                    var pgUser = pgUserPass.Split(":")[1].Split("//")[1];
                     var pgPass = pgUserPass.Split(":")[2];
                     var pgHost = pgHostPort.Split(":")[0];
                     var pgPort = pgHostPort.Split(":")[1];
