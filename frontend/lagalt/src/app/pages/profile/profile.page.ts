9a2ce4b6aeb8adf9500c92909cf06ca76d980cbf
emilons <emilons@hotmail.com>
2021-10-13 13:34:02 +0200

    #39 changed state management with user service

--- frontend/lagalt/src/app/pages/profile/profile.page.ts
+++ frontend/lagalt/src/app/pages/profile/profile.page.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { User } from 'src/app/models/profile.model'
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model'
 import { NgForm, ReactiveFormsModule } from '@angular/forms';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { UserService } from 'src/app/services/user.service';
@@ -10,71 +10,67 @@ import { Observable, Subscription } from 'rxjs';
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.css']
 })
-export class ProfilePage implements OnInit {
+export class ProfilePage implements OnInit, OnDestroy {
 
-  public skills : [string] = ['Using LagAlt']
+  private user$: Subscription;
+  public user: User = {
+    id: 0,
+    userName: '',
+    description: '',
+    image: '',
+    portfolio: '',
+    skills: [],
+    projects: []
+  }
   public isChecked = true;
   public color = 'accent';
-  public description : string = ""
 
   constructor(private readonly userService : UserService) {
-
-
+    this.user$ = this.userService.user$.subscribe((user: User) => {
+      this.user = user;
+    })
   }
 
   ngOnInit(): void {
-    this.userService.getUsers()
-    this.userService.getUserById(1)
-    this.userById$.subscribe(data => {
-      this.description = data.description
-    })
+    this.userService.getUserById(1) // CHANGE TO IMPLEMENT ON LOGIN
   }
+  ngOnDestroy(): void {
+    this.user$.unsubscribe();
+  }
+
   //Changes the hidden mode button from true to false or false to true
   changed(){
     console.log(this.isChecked);
   }
+
+
   //Adds skill to a list in the user profile
   addSkill(addSkillForm : NgForm){
-    if(addSkillForm){
-      this.skills.push(addSkillForm.value.skills);
-    }
-    let usersResults : User[] = []
-    this.userService.getUsers$().subscribe(data => {
-      console.log(data);
-      usersResults = data
-    })
-    let usersResultsById : User
-    this.userService.getUserById$().subscribe(data => {
-      console.log(data);
-      usersResultsById = data
-      let description = data.description
-      console.log(description);
 
-    })
-  }
+    // WAIT WITH IMPLEMENTING TILL USER MODEL IS UPDATED with skill[] instead of array of ids
 
-
-
-  handleDescription(handleDescriptionForm : NgForm){
-    let newDescription = handleDescriptionForm.value.description
-    this.userService.getUserById$().subscribe(data => {
-      data.description = newDescription
-
-    })
-    //this.userService.putUserById$(textAreaForm.value.description)
-    // let userDescription = this.userService.getUserById$().subscribe(data => {
-    //   data.description
-    //   console.log("USR ",userDescription);
-
+    // if(addSkillForm){
+    //   this.skills.push(addSkillForm.value.skills);
+    // }
+    // let usersResults : User[] = []
+    // this.userService.getUsers$().subscribe(data => {
+    //   console.log(data);
+    //   usersResults = data
+    // })
+    // let usersResultsById : User
+    // this.userService.getUserById$().subscribe(data => {
+    //   console.log(data);
+    //   usersResultsById = data
+    //   let description = data.description
+    //   console.log(description);
     // })
   }
 
+  handleDescription(handleDescriptionForm : NgForm){
+    // maybe check that description is changed, and that description is not equal to empty string or white spaces
+    this.user.description = handleDescriptionForm.value.description
+    // RUN PUT USER API CALL so we can update description
+  }
 
-  get users$(): Observable<User[]> {
-    return this.userService.getUsers$();
-  }
-  get userById$(): Observable<User> {
-    return this.userService.getUserById$();
-  }
 
 }
--- frontend/lagalt/src/app/services/user.service.ts
+++ frontend/lagalt/src/app/services/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable} from "@angular/core";
 import { HttpClient, HttpErrorResponse, HttpHeaders } from "@angular/common/http";
-import { User } from "../models/profile.model";
+import { User } from "../models/user.model";
 import { BehaviorSubject, Observable, Subscription } from 'rxjs';
 import { finalize, map, retry, switchMap, tap } from "rxjs/operators";
 import { environment } from "src/environments/environment";
@@ -22,48 +22,35 @@ const defaultUser : User = {
 })
 export class UserService {
 
-  private readonly _users$: BehaviorSubject<User[]> = new BehaviorSubject<User[]>([]);
-  private readonly _user$: BehaviorSubject<User> = new BehaviorSubject<User>(defaultUser);
+  public readonly users$: BehaviorSubject<User[]> = new BehaviorSubject<User[]>([]);
+  public readonly user$: BehaviorSubject<User> = new BehaviorSubject<User>(defaultUser);
+  
   constructor(private readonly http : HttpClient) { }
 
   // State CRUD functions
-
-  public getUsers$(): Observable<User[]> {
-    return this._users$.asObservable()
-  }
-  public getUserById$(): Observable<User> {
-    return this._user$.asObservable()
-  }
-  public putUserById$(user: User): Observable<User> {
-    return this._user$.asObservable()
-  }
-  public postNewUser$(): Observable<User[]> {
-    return this._users$.asObservable()
-  }
-  public setUser(user: User[]): void {
-    this._users$.next(user)
+  public setUsers(users: User[]): void {
+    this.users$.next(users)
   }
   public setUserById(user: User): void {
-    this._user$.next(user)
+    this.user$.next(user)
   }
+
    // API CRUD calls
   public getUsers(): Subscription {
     //set users as enum in storage here.
     return this.http.get<User[]>(API_URL_USERS)
-        .subscribe((user: User[]) => {
-            this.setUser(user)
-
+        .subscribe((users: User[]) => {
+            this.setUsers(users)
         });
   }
+
   public getUserById(id : number): Subscription {
     //set users as enum in storage here.
     return this.http.get<User>(API_URL_USERS +`/${id}`)
         .subscribe((user: User) => {
             this.setUserById(user)
-
         });
   }
-
 
   // public putDescriptionById(id : number , description : string) : Subscription {
   //   return this.http.put<User>(API_URL_USERS +`/${id}`, description)
