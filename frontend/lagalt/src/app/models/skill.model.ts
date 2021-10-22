83fb8afe3bb2d58c72b385e557429e3b2be61acd
rahul.walia.singh@no.experis.com <rahul.walia.singh@no.experis.com>
2021-10-22 08:57:09 +0200

    #56 feat user can now see which skills match for a project

--- frontend/lagalt/src/app/models/skill.model.ts
+++ frontend/lagalt/src/app/models/skill.model.ts
@@ -1,13 +1,6 @@
 import { Project } from "./project.model";
 import { UserComplete } from "./user/user-complete.model";
 
-export interface PostSkill {
-    id: number,
-    name: string,
-    user: number[],
-    project: number[]
-}
-
 export interface Skill{
     id: number,
     name: string
