0aff84ca6e552c5cf84a98a7437875d38e2b134d
emilons <emilons@hotmail.com>
2021-10-22 15:35:05 +0200

    added my-project service, removed renderproject

---
+++ frontend/lagalt/src/app/services/my-project.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Project } from "../models/project.model";
+import { BehaviorSubject, Subscription } from 'rxjs';
+import { environment } from "src/environments/environment";
+import { ProjectPageWrapper } from "../models/project-page-wrapper.model";
+
+const API_URL = `${environment.apiUrl}Projects`;
+@Injectable({
+  providedIn: 'root'
+})
+export class MyProjectService {
+
+  // Store observables
+  public readonly myProjects$: BehaviorSubject<Project[]> = new BehaviorSubject<Project[]>([]);
+
+  // Page properties
+  public offset = 0;
+  public limit = 2;
+  public pages = 0;
+  public currentPage = 1;
+  public totalEntities = 0;
+
+  constructor(private readonly http : HttpClient) {
+  }
+
+  // State CRUD functions
+  public setMyProjects(projects: Project[]): void {
+      this.myProjects$.next(projects)
+  }
+
+  // Page modifiers
+  nextPage(userId: number): void {
+      this.offset += this.limit;
+      this.currentPage++;
+      this.getProjectsByUserId(userId);
+  }
+  prevPage(userId: number): void {
+      this.offset -= this.limit;
+      this.currentPage--;
+      this.getProjectsByUserId(userId);
+  }
+
+  // API CRUD calls
+  public getProjectsByUserId(userId : number): Subscription {
+      return this.http.get<ProjectPageWrapper>(`${API_URL}/User/${userId}`)
+          .subscribe((project: ProjectPageWrapper) => {
+              this.setMyProjects(project.results)
+              this.pages = Math.ceil(this.totalEntities/this.limit)
+          });
+  }
+}
