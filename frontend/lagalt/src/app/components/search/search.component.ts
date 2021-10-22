d26078ed449b272f412e63defe4dfe0b8c21e49c
emilons <emilons@hotmail.com>
2021-10-22 15:55:16 +0200

    idk

--- frontend/lagalt/src/app/components/search/search.component.ts
+++ frontend/lagalt/src/app/components/search/search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { MyProjectService } from 'src/app/services/my-project.service';
 import { Project } from "../../models/project.model";
 import { ProjectService } from "../../services/project.service"
 
@@ -12,7 +13,9 @@ import { ProjectService } from "../../services/project.service"
 export class SearchComponent implements OnInit, OnDestroy {
 
 
-  constructor(private readonly projectService : ProjectService, private router: Router) {
+  constructor(private readonly projectService : ProjectService,
+    private readonly myProjectService: MyProjectService, 
+    private router: Router) {
   }
 
   ngOnInit(): void {
@@ -27,7 +30,7 @@ export class SearchComponent implements OnInit, OnDestroy {
     this.projectService.projects$.subscribe(data => {
       searchResults = data.filter(p => p.title.toLowerCase().includes(searchProjectForm.value.searchInput.toLowerCase()))
     }).unsubscribe()
-    this.projectService.setProjects(searchResults)
+    this.handleSetRenderedProjects(searchResults)
   }
 
   searchMusic(searchProjectForm : NgForm): void {
@@ -36,7 +39,7 @@ export class SearchComponent implements OnInit, OnDestroy {
     this.projectService.projects$.subscribe(data => {
       searchResults = data.filter(p => p.title.includes(searchProjectForm.value.searchInput) && p.profession.id === 1)
     }).unsubscribe()
-    this.projectService.setProjects(searchResults)
+    this.handleSetRenderedProjects(searchResults)
   }
 
   searchFilm(searchProjectForm : NgForm): void {
@@ -45,7 +48,7 @@ export class SearchComponent implements OnInit, OnDestroy {
     this.projectService.projects$.subscribe(data => {
       searchResults = data.filter(p => p.title.includes(searchProjectForm.value.searchInput) && p.profession.id === 2)
     }).unsubscribe()
-    this.projectService.setProjects(searchResults)
+    this.handleSetRenderedProjects(searchResults)
   }
 
   searchGame(searchProjectForm : NgForm): void {
@@ -54,7 +57,7 @@ export class SearchComponent implements OnInit, OnDestroy {
     this.projectService.projects$.subscribe(data => {
       searchResults = data.filter(p => p.title.includes(searchProjectForm.value.searchInput) && p.profession.id === 3)
     }).unsubscribe()
-    this.projectService.setProjects(searchResults)
+    this.handleSetRenderedProjects(searchResults)
   }
 
   searchWeb(searchProjectForm : NgForm): void {
@@ -63,13 +66,20 @@ export class SearchComponent implements OnInit, OnDestroy {
     this.projectService.projects$.subscribe(data => {
       searchResults = data.filter(p => p.title.includes(searchProjectForm.value.searchInput) && p.profession.id === 4)
     }).unsubscribe()
-    this.projectService.setProjects(searchResults)
-    
-    
+    this.handleSetRenderedProjects(searchResults)
   }
-  
+
+  handleSetRenderedProjects(searchResults: Project[]) {
+    if (this.router.url === "/main") {
+      this.projectService.setProjects(searchResults)
+    }
+    else {
+      this.myProjectService.setMyProjects(searchResults)
+    }
+  }
+
   handleRouteChange() {
-    if (this.router.url !== "main" && this.router.url !== "my-projects") {
+    if (this.router.url !== "/main" && this.router.url !== "/my-projects") {
       this.router.navigate(['main'])
     }
   }
