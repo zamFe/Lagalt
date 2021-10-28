3da9d9f210c0258e6f4f79f721afcb9ae9b5c1a3
Edlix <56119942+Edlix@users.noreply.github.com>
2021-10-28 10:08:02 +0200

    Style changes

--- frontend/lagalt/src/app/components/login-button/login-button.component.ts
+++ frontend/lagalt/src/app/components/login-button/login-button.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-login-button',
   templateUrl: './login-button.component.html',
-  styles: [],
+  styleUrls: ['./login-button.component.css']
 })
 export class LoginButtonComponent implements OnInit {
   constructor(public auth: AuthService) {}
